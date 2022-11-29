2c678f21804b2f01a088c9cbc7c6b881222a4823
viannejohn <95766596+viannejohn@users.noreply.github.com>
2022-11-29 17:00:10 +0300

    Add files via upload

---
+++ sketch_nov4b/sketch_nov4b.ino
@@ -0,0 +1,20 @@
+/*
+  Blinking LED project.
+  Turns on an LED on for one second, then off for one second, repeatedly.
+  Last Modification modified 30 March 2020
+  by Roy Ben Avraham
+ */
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
